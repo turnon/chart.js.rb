e0dea5a5c448186a7adc41ee2c8e8947a971ad3f
ken <block24block@gmail.com>
2016-06-22 17:21:02 +0800

    can filter xy

--- test/test_my_chart.rb
+++ test/test_my_chart.rb
@@ -8,16 +8,23 @@ class TestMyChart < MiniTest::Unit::TestCase
     assert_equal [1,2,3,4,5,6,7,8,9,10], @mc1.value(MyChart::ALL_DATA)
   end
 
-  def test_select
+  def test_select_from_material
     assert_equal [3,4,5,6,7,8,9,10], @mc.value(:ge3)
     assert_equal [2,4,6,8,10], @mc.value(:x2)
+  end
+
+  def test_select_from_selected
     assert_equal [4,6,8,10], @mc.value(:even_from_ge3)
   end
 
-  def test_group_by
+  def test_x_to_xy
     assert_equal ({'even' => [2,4,6,8,10], 'odd' => [1,3,5,7,9]}), @mc.value(:odd_or_even)
     assert_equal ({'even' => [4,6,8,10], 'odd' => [3,5,7,9]}), @mc.value(:ge3_into_odd_or_even)
     assert_equal ({'divisible_by_3' => [6], 'not_divisible_by_3' => [4,8,10]}), @mc.value(:even_from_ge3_into_divisible_by_3)
+  end
+
+  def test_select_from_xy
+    assert_equal ({1 => [1,4,7,10], 2 => [2,5,8]}), @mc.value(:not_divisible_by_3_from_mod3)
   end
 
   def setup
@@ -44,6 +51,14 @@ class TestMyChart < MiniTest::Unit::TestCase
 
       group by: :odd_or_even do |n|
         n.odd? ? 'odd' : 'even'
+      end
+
+      group by: :mod3 do |n|
+        n % 3
+      end
+
+      select :not_divisible_by_3, from: :mod3 do |mod3|
+        not mod3.zero?
       end
 
     end
