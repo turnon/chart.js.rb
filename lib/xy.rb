1c34d21eed27a3b8898f83fc0888570142c3a76c
k <k@wh.com>
2016-06-22 17:21:02 +0800

    can filter xy

--- lib/xy.rb
+++ lib/xy.rb
@@ -3,10 +3,10 @@ class XY
     @objs_hash = objs_hash
   end
 
-  #def select &blk
-  #  X.new value.select(&blk)
-  #end
-  #
+  def select &blk
+    XY.new value.select(&blk)
+  end
+
   #def group_by &blk
   #  XY.new value.group_by(&blk)
   #end
