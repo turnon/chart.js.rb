f7e19cee375831168d17f8700ad6334bb065db3d
ken <block24block@gmail.com>
2016-07-19 16:54:55 +0800

    extract GroupByARGV & SelectFromARGV

--- lib/my_chart.rb
+++ lib/my_chart.rb
@@ -1,5 +1,8 @@
 require 'tasks'
 require 'x'
+require 'my_chart_group_by_argv'
+require 'my_chart_select_from_argv'
+require 'x'
 require 'erb'
 require 'chart/proto'
 
@@ -62,59 +65,6 @@ module MyChart
 
     def value name
       @tasks[name.to_sym].result.value
-    end
-
-    class GroupByARGV
-
-      attr_reader :material_id, :production_id
-
-      def initialize *args
-        if material_specified? args
-          @material_id = args[0]
-          @production_id = concat_production_id @material_id, args[1][:by]
-        elsif material_not_specified? args
-          @material_id = ALL_DATA
-          @production_id = [:GROUP, :BY, args[0][:by]].join('_').to_sym
-        else
-          raise "invalid arguments for group_by : #{args}"
-        end
-      end
-
-      private
-
-      def material_specified? args
-        args.size == 2 and args[1].kind_of? Hash and args[1][:by]
-      end
-
-      def material_not_specified? args
-        args.size == 1 and args[0].kind_of? Hash and args[0][:by]
-      end
-
-      def concat_production_id material, by
-        string = if material =~ /^GROUP/
-                   [material, :AND_THEN, :GROUP, :BY, by]
-                 else
-                   [:GROUP, material, :BY, by]
-                 end
-        string.join('_').to_sym
-      end
-    end
-
-    class SelectFromARGV
-
-      attr_reader :material_id, :production_id
-
-      def initialize *args
-        if args.size == 2 and args[1].kind_of? Hash and args[1][:from]
-          @material_id = args[1][:from]
-          @production_id = [args[0], :FROM, material_id].join('_').to_sym
-        elsif args.size == 1
-          @material_id = ALL_DATA
-          @production_id = args[0]
-        else
-          raise "invalid arguments for select : #{args}"
-        end
-      end
     end
 
     private
