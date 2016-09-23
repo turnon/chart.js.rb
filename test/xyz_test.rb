0b03ba54bac753ecc29d3e8d53e0683498426fd2
ken <block24block@gmail.com>
2016-09-23 11:04:00 +0800

    default sort by key on y-axis

--- test/xyz_test.rb
+++ test/xyz_test.rb
@@ -13,8 +13,8 @@ class TestXYZ < MiniTest::Unit::TestCase
   end
 
   def setup
-    @xyz = XYZ.new({0 => {'even' => [6,12,18], 'odd' => [3,9,15]},
-           1 => {'even' => [4,10,16], 'odd' => [1,7,13,19]},
-           2 => {'even' => [2,8,14,20], 'odd' => [5,11,17]}})
+    @xyz = XYZ.new({0 => {'odd' => [3,9,15], 'even' => [6,12,18]} ,
+           1 => {'odd' => [1,7,13,19], 'even' => [4,10,16]},
+           2 => {'odd' => [5,11,17], 'even' => [2,8,14,20]}})
   end
 end
