db56ac596d50aeb3c2c474ff8236aa7231bd0861
ken <block24block@gmail.com>
2017-02-01 15:41:43 +0800

    \t to \s

--- lib/my_chart/dsl/draw.rb
+++ lib/my_chart/dsl/draw.rb
@@ -7,13 +7,13 @@ module MyChart
       MyChart::Type.each_sym do |chart_cmd|
 
         define_method chart_cmd do |*arg|
-  	chart_config = ChartCmdARGV.new *arg
+          chart_config = ChartCmdARGV.new *arg
 
-  	chart_id = "#{chart_cmd}__#{chart_config.data_id}".to_sym
-  	klass = MyChart::Type.concrete chart_cmd
-  	grp_data = grouped chart_config
+          chart_id = "#{chart_cmd}__#{chart_config.data_id}".to_sym
+          klass = MyChart::Type.concrete chart_cmd
+          grp_data = grouped chart_config
 
-  	charts[chart_id] = klass.new grp_data, id: chart_id, w: chart_config.w, h: chart_config.h
+          charts[chart_id] = klass.new grp_data, id: chart_id, w: chart_config.w, h: chart_config.h
         end
       end
 
@@ -46,29 +46,29 @@ module MyChart
         attr_reader :opt, :x, :y
 
         def initialize *arg
-  	return if arg.empty?
+          return if arg.empty?
           @opt = (arg[-1].kind_of? Hash) ? arg[-1] : {}
-  	@x = arg[0] if arg[0].kind_of? Symbol
-  	@y = arg[1] if arg[1] and arg[1].kind_of? Symbol
+          @x = arg[0] if arg[0].kind_of? Symbol
+          @y = arg[1] if arg[1] and arg[1].kind_of? Symbol
         end
 
         def method_missing name, *arg
-  	return opt[name] if [:w, :h, :from, :keys, :asc, :desc].include? name
-  	super
+          return opt[name] if [:w, :h, :from, :keys, :asc, :desc].include? name
+          super
         end
 
         def sort
-  	order = (asc and :asc) or (desc and :desc)
-  	[order, 'by', (asc or desc)].join('_') if order
+          order = (asc and :asc) or (desc and :desc)
+          [order, 'by', (asc or desc)].join('_') if order
         end
 
         def data_id
-  	[x,
+          [x,
            y ? y : "no_y",
-  	 keys ? "keys_#{keys.hash}" : "no_keys",
-  	 from ? "from_#{from}" : "from_all",
-  	 sort
-  	].compact.join '__'
+           keys ? "keys_#{keys.hash}" : "no_keys",
+           from ? "from_#{from}" : "from_all",
+           sort
+          ].compact.join '__'
         end
       end
 
